89607f15d5dd074c0d7552be7010bae1dd850fd9
ITTYwei <891958211@qq.com>
2026-01-11 15:38:19 +0800

    添加存储服务

---
+++ shiguangji-oss/shiguangji-oss-biz/src/main/java/com/xiaowei/shiguangji/oss/biz/ShiguangjiOssBizApplication.java
@@ -0,0 +1,15 @@
+package com.xiaowei.shiguangji.oss.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ShiguangjiOssBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShiguangjiOssBizApplication.class, args);
+    }
+}
