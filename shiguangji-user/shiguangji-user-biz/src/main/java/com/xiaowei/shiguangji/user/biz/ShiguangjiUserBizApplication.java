22600ad7bfc3392a3fece60430436a1696485002
ITTYwei <891958211@qq.com>
2026-01-15 18:34:36 +0800

    用户服务构建

---
+++ shiguangji-user/shiguangji-user-biz/src/main/java/com/xiaowei/shiguangji/user/biz/ShiguangjiUserBizApplication.java
@@ -0,0 +1,17 @@
+package com.xiaowei.shiguangji.user.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ShiguangjiUserBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiguangjiUserBizApplication.class, args);
+    }
+
+}
