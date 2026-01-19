2d4ee993d308c205282c7b37b55265d53eced039
ITTYwei <891958211@qq.com>
2026-01-20 00:10:31 +0800

    创建kv服务

---
+++ shiguangji-kv/shiguangji-kv-biz/src/main/java/com/xiaowei/shiguangji/kv/biz/ShiguangjiKVBizApplication.java
@@ -0,0 +1,17 @@
+package com.xiaowei.shiguangji.kv.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: 魏玉石
+ * @data: 2026/1/20
+ * @description:
+ */
+@SpringBootApplication
+public class ShiguangjiKVBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShiguangjiKVBizApplication.class, args);
+    }
+
+}
